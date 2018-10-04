5f47ea34121093af06e499681ad087054ca9ff54
Unknown <sam.masaki@gmail.com>
2018-10-04 14:03:00 -0700

    Visual Changes to Keymaps and Reorganization
    Added more keycode defines for keys in temporary layers that used to be transparent so that they are now KC_NO, since having them be transparent was at best useless for me, and at worst easy to accidentally press stuff. Also moved some enums to the header file.

--- keyboards/dz60/keymaps/smski/keymap.c
+++ keyboards/dz60/keymaps/smski/keymap.c
@@ -3,35 +3,9 @@
 #define HUE_STEP 45
 #define SAT_STEP 51
 #define VAL_STEP 51
-
-enum custom_keycodes {
-	DZ_HU_D = SAFE_RANGE,
-	DZ_HU_I,
-	DZ_SA_D,
-	DZ_SA_I,
-	DZ_VA_D,
-	DZ_VA_I,
-	DZ_VERS,
-	DZ_QGML,
-	DZ_QWER,
-	DZ_DBUG,
-	DZ_CYCL,
-};
-
-enum custom_layers {
-	L_QG = 0,
-	L_QW,
-	L_NM,
-	L_TH,
-	L_FN,
-	L_RG
-};
-
-enum underglowProp {
-	HUE = 0,
-	SAT,
-	VAL
-};
+#define ___X___ KC_NO
+#define ___E___ KC_NO
+#define _CURMOD KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -56,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KC_BSPC, KC_D,    KC_S,    KC_T,    KC_N,    KC_R,    KC_I,    KC_A,    KC_E,    KC_O,    KC_H,    KC_QUOT,                   KC_ENT,
 
-	KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_J,    KC_K,    KC_P,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, DZ_CYCL,
+	KC_LSFT, ___E___, KC_Z,    KC_X,    KC_C,    KC_V,    KC_J,    KC_K,    KC_P,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, DZ_CYCL,
 
-	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           MO(L_TH),         KC_DEL,                    KC_RALT, MO(L_FN),XXXXXXX, MO(L_RG),KC_RCTL),
+	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           MO(L_TH),         KC_DEL,                    KC_RALT, MO(L_FN),___E___, MO(L_RG),KC_RCTL),
 
 	/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -83,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
 
-	KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, DZ_CYCL,
+	KC_LSFT, ___E___, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, DZ_CYCL,
 
-	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           MO(L_TH),         KC_DEL,                    KC_RALT, MO(L_FN),XXXXXXX, MO(L_RG),KC_RCTL),
+	KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,           MO(L_TH),         KC_DEL,                    KC_RALT, MO(L_FN),___E___, MO(L_RG),KC_RCTL),
 
 	/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -104,15 +78,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*/
 
 [L_NM] = LAYOUT(
-	_______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P7,   KC_P7,   _______, _______, _______, _______, _______,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_P7,   KC_P7,   KC_P7,   ___X___, ___X___, ___X___, _CURMOD, ___X___,
 
-	_______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   _______, _______, _______,          _______,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_P4,   KC_P5,   KC_P6,   ___X___, ___X___, ___X___,          ___X___,
 
-	_______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,                    KC_ENT,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_P1,   KC_P2,   KC_P3,   ___X___, ___X___,                    KC_ENT,
 
-	_______, XXXXXXX, _______, _______, _______, _______, _______, _______, KC_P0,   _______, KC_PDOT, _______,          _______, _______,
+	___X___, ___E___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_P0,   ___X___, KC_PDOT, ___X___,          ___X___, ___X___,
 
-	_______, _______, _______, _______,          _______,                   _______,          _______, _______, XXXXXXX, _______, _______),
+	___X___, ___X___, ___X___, ___X___,          ___X___,                   ___X___,          ___X___, ___X___, ___E___, ___X___, ___X___),
 
 	/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -131,15 +105,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*/
 
 [L_TH] = LAYOUT(
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
 
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,
 
-	_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,                   _______,
+	___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ___X___,                   ___X___,
 
-	_______, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+	___X___, ___E___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___, ___X___,
 
-	_______, _______, _______, _______,          _______,                   _______,          _______, XXXXXXX, XXXXXXX, XXXXXXX, _______),
+	___X___, ___X___, ___X___, ___X___,          _CURMOD,                   ___X___,          ___X___, XXXXXXX, ___E___, XXXXXXX, ___X___),
 
 	/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -158,15 +132,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*/
 
 [L_FN] = LAYOUT(
-	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+	KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ___X___, ___X___,
 
-	_______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+	___X___, KC_MPRV, KC_MPLY, KC_MNXT, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___,
 
-	_______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+	___X___, KC_VOLD, KC_MUTE, KC_VOLU, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   ___X___,
 
-	_______, XXXXXXX, _______, _______, _______, _______, _______, _______, DZ_QWER, DZ_QGML, _______, _______,          _______, _______,
+	___X___, ___E___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, DZ_QWER, DZ_QGML, ___X___, ___X___,          ___X___, ___X___,
 
-	_______, _______, _______, _______,          XXXXXXX,                   _______,          _______, _______, XXXXXXX, XXXXXXX, _______),
+	___X___, ___X___, ___X___, ___X___,          XXXXXXX,                   ___X___,          ___X___, _CURMOD, ___E___, XXXXXXX, ___X___),
 
 	/*-----------------------------------------------------------------------------------------------------------------------------------*/
 
@@ -185,15 +159,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	*/
 
 [L_RG] = LAYOUT(
-	RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, _______, _______, _______, XXXXXXX, RESET,
+	RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,RGB_M_SN,RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, ___X___, ___X___, ___X___, XXXXXXX, RESET,
 
-	DZ_HU_D, RGB_HUD, RGB_HUI, DZ_HU_I, _______, _______, _______, _______, _______, _______, _______, RGB_RMOD,RGB_MOD,          _______,
+	DZ_HU_D, RGB_HUD, RGB_HUI, DZ_HU_I, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, RGB_RMOD,RGB_MOD,          ___X___,
 
-	DZ_SA_D, RGB_SAD, RGB_SAI, DZ_SA_I, _______, _______, _______, _______, _______, _______, _______, _______,                   DZ_DBUG,
+	DZ_SA_D, RGB_SAD, RGB_SAI, DZ_SA_I, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,                   DZ_DBUG,
 
-	DZ_VA_D, XXXXXXX, RGB_VAD, RGB_VAI, DZ_VA_I, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+	DZ_VA_D, ___E___, RGB_VAD, RGB_VAI, DZ_VA_I, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,          ___X___, ___X___,
 
-	DZ_VERS, _______, _______, _______,          XXXXXXX,                   _______,          _______, XXXXXXX, XXXXXXX, _______, _______),
+	DZ_VERS, ___X___, ___X___, ___X___,          XXXXXXX,                   ___X___,          ___X___, XXXXXXX, ___E___, _CURMOD, ___X___),
 };
 
 rgblight_config_t origGlowState; // Original user-set underglow state
--- keyboards/dz60/keymaps/smski/keymap.h
+++ keyboards/dz60/keymaps/smski/keymap.h
@@ -1,8 +1,34 @@
 #ifndef SMSKI_H
 #include QMK_KEYBOARD_H
 
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+enum custom_keycodes {
+	DZ_HU_D = SAFE_RANGE,
+	DZ_HU_I,
+	DZ_SA_D,
+	DZ_SA_I,
+	DZ_VA_D,
+	DZ_VA_I,
+	DZ_VERS,
+	DZ_QGML,
+	DZ_QWER,
+	DZ_DBUG,
+	DZ_CYCL,
+};
+
+enum custom_layers {
+	L_QG = 0,
+	L_QW,
+	L_NM,
+	L_TH,
+	L_FN,
+	L_RG
+};
+
+enum underglowProp {
+	HUE = 0,
+	SAT,
+	VAL
+};
 
 void flashGlow(rgblight_config_t tempState, uint16_t hue, uint16_t duration);
 void lockGlow(rgblight_config_t tempState);
