342727fc11fe797821a76c65a6324f355ff70e83
Sam Lewis <sam.masaki@pm.me>
2021-03-24 19:56:28 -0700

    Made lock state more compact

--- keyboards/lily58/lib/host_led_state_reader.c
+++ keyboards/lily58/lib/host_led_state_reader.c
@@ -3,14 +3,14 @@
 #include "host.h"
 #include "lily58.h"
 
-char host_led_state_str[24];
+char host_led_state_str[6];
 
 const char *read_host_led_state(void)
 {
-  snprintf(host_led_state_str, sizeof(host_led_state_str), "NL:%s CL:%s SL:%s",
-           (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) ? "on" : "- ",
-           (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) ? "on" : "- ",
-           (IS_HOST_LED_ON(USB_LED_SCROLL_LOCK)) ? "on" : "- ");
+  snprintf(host_led_state_str, sizeof(host_led_state_str), "%c %c %c",
+           (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) ? 'C' : '_',
+           (IS_HOST_LED_ON(USB_LED_NUM_LOCK)) ? 'N' : '_',
+           (IS_HOST_LED_ON(USB_LED_SCROLL_LOCK)) ? 'S' : '_');
 
   return host_led_state_str;
 }
